40bac4482631351ac0ebff193a3749e017a8517e
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-04-04 02:48:24 +0200

    chore: add imports to parser __init__s

--- src/mkdown/parsers/github_api_parser/__init__.py
+++ src/mkdown/parsers/github_api_parser/__init__.py
@@ -1,1 +1,5 @@
 """GitHub API Markdown Parser."""
+
+from mkdown.parsers.github_api_parser.parser import GithubApiParser
+
+__all__ = ["GithubApiParser"]
--- src/mkdown/parsers/pyromark_parser/__init__.py
+++ src/mkdown/parsers/pyromark_parser/__init__.py
@@ -1,1 +1,5 @@
 """Pyromark Parser."""
+
+from mkdown.parsers.pyromark_parser.parser import PyroMarkParser
+
+__all__ = ["PyroMarkParser"]
