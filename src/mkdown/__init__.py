8373ffb254bd95877ba1ffc3bbb9d1bb951b6ae9
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-10-06 18:23:26 +0200

    chore: bump template

--- src/mkdown/__init__.py
+++ src/mkdown/__init__.py
@@ -1,10 +1,17 @@
-"""Markdown document models and utilities."""
+"""mkdown: Tools for (Python-)Markdown"""
 
 from __future__ import annotations
 
 from importlib.metadata import version
 
 __version__ = version("mkdown")
+__title__ = "mkdown"
+__description__ = "Tools for (Python-)Markdown"
+__author__ = "Philipp Temminghoff"
+__author_email__ = "philipptemminghoff@googlemail.com"
+__copyright__ = "Copyright (c) 2024 Philipp Temminghoff"
+__license__ = "MIT"
+__url__ = "https://github.com/phil65/mkdown"
 
 from mkdown.models.document import Document
 from mkdown.models.textchunk import TextChunk
